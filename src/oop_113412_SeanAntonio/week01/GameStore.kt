86caecce86fc9fa84e9634caf88cff1d01c9cfa9
Sean Antonio <seanantonio53@gmail.com>
2026-02-03 23:08:18 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_113412_SeanAntonio/week01/GameStore.kt
+++ src/oop_113412_SeanAntonio/week01/GameStore.kt
@@ -4,6 +4,15 @@ fun main() {
     val judul = "Elden Ring"
     val harga = 599000
 
+    val hargaAkhir = calculateDiscount(harga)
+
     println("Judul Game: $judul")
-    println("Harga: Rp $harga")
+    println("Harga Asli: Rp $harga")
+    println("Harga Akhir: Rp $hargaAkhir")
 }
+
+fun calculateDiscount(harga: Int): Int =
+    if (harga > 500_000)
+        harga - (harga * 20 / 100)
+    else
+        harga - (harga * 10 / 100)
