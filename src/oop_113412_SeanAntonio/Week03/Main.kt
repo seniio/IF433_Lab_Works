5b308290b01edeee93fa208505459fec244fe4e0
Sean Antonio <sean.antonio888@gmail.com>
2026-02-20 15:04:21 +0700

    week03: (task 2) implement Player testing in main

--- src/oop_113412_SeanAntonio/Week03/Main.kt
+++ src/oop_113412_SeanAntonio/Week03/Main.kt
@@ -1,12 +1,21 @@
 package oop_113412_SeanAntonio.Week03
 
 fun main() {
-    val w = Weapon("Excalibur")
 
+    val w = Weapon("Excalibur")
     w.damage = -50
     w.damage = 9999
-
     println("Weapon: ${w.name}")
     println("Damage: ${w.damage}")
     println("Tier: ${w.tier}")
+
+    println("-----------")
+
+    val player = Player("Sean")
+
+    player.addXp(50)
+    println("Level sekarang: ${player.level}")
+
+    player.addXp(60) // total 110 → Level 2
+    println("Level sekarang: ${player.level}")
 }
