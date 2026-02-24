b5f595e21aebccfb1b9373a5445d5410a0beae79
Sean Antonio <sean.antonio888@gmail.com>
2026-02-24 15:29:38 +0700

    week04: (task 2) create Developer class

---
+++ src/oop_113412_SeanAntonio/Week04/Developer.kt
@@ -0,0 +1,12 @@
+package oop_113412_SeanAntonio.Week04
+
+class Developer(
+    name: String,
+    baseSalary: Int,
+    val programmingLanguage: String
+) : Employee(name, baseSalary) {
+
+    override fun work() {
+        println("$name sedang ngoding menggunakan $programmingLanguage.")
+    }
+}
