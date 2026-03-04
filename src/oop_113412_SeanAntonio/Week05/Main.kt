0467b95be8d56ab992c247c99ecf3db7c0073924
Sean Antonio <sean.antonio888@gmail.com>
2026-03-04 23:40:44 +0700

    week05: (task 2) implement payment polymorphism

---
+++ src/oop_113412_SeanAntonio/Week05/Main.kt
@@ -0,0 +1,14 @@
+package oop_113412_SeanAntonio.Week05
+
+fun main() {
+
+    val eWallet = EWallet("Sean E-Wallet", 50000.0)
+    val creditCard = CreditCard("Sean Credit Card", 100000.0)
+
+    val paymentMethods: List<PaymentMethod> = listOf(eWallet, creditCard)
+
+    for (method in paymentMethods) {
+        println("\nMemproses pembayaran 75000.0 untuk ${method.accountName}")
+        method.processPayment(75000.0)
+    }
+}
