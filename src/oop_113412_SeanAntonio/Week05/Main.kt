f989ac59d781fd8b7f9a7ab1734c1e4b0c56c34f
Sean Antonio <sean.antonio888@gmail.com>
2026-03-04 23:27:07 +0700

    week05: (task 1) test MathHelper overloading in main

---
+++ src/oop_113412_SeanAntonio/Week05/Main.kt
@@ -0,0 +1,14 @@
+package oop_113412_SeanAntonio.Week05
+
+fun main() {
+
+    val mathHelper = MathHelper()
+
+    val luasPersegi = mathHelper.hitungLuas(4)
+    val luasPersegiPanjang = mathHelper.hitungLuas(5, 3)
+    val luasLingkaran = mathHelper.hitungLuas(7.0)
+
+    println("Luas Persegi: $luasPersegi")
+    println("Luas Persegi Panjang: $luasPersegiPanjang")
+    println("Luas Lingkaran: $luasLingkaran")
+}
--- src/oop_113412_SeanAntonio/Week05/MathHelper.kt
+++ src/oop_113412_SeanAntonio/Week05/MathHelper.kt
@@ -2,17 +2,14 @@ package oop_113412_SeanAntonio.Week05
 
 class MathHelper {
 
-    // Luas persegi
     fun hitungLuas(sisi: Int): Int {
         return sisi * sisi
     }
 
-    // Luas persegi panjang
     fun hitungLuas(panjang: Int, lebar: Int): Int {
         return panjang * lebar
     }
 
-    // Luas lingkaran
     fun hitungLuas(jariJari: Double): Double {
         return 3.14 * jariJari * jariJari
     }
