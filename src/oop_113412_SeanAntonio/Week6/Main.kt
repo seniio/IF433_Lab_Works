39cba3ee49afd00cbf8a3e93d1f9f68d063ead82
Sean Antonio <sean.antonio888@gmail.com>
2026-03-10 15:32:58 +0700

    week06: (task) test smart home system logic in main

--- src/oop_113412_SeanAntonio/Week6/Main.kt
+++ src/oop_113412_SeanAntonio/Week6/Main.kt
@@ -1,7 +1,20 @@
 package oop_113412_SeanAntonio.Week6
 
-fun Main {
+fun main() {
+
     val lamp = SmartLamp("L1", "Ruang Tamu")
     val speaker = SmartSpeaker("S1", "Google Nest Dapur")
     val cctv = SmartCCTV("C1", "Ezviz Garasi")
+
+    val hub = SmartHomeHub()
+
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
+
+    hub.activateSecurityMode()
+
+    println("----")
+
+    hub.turnOffAllSwitches()
 }
