20619be964dc698968121c8f4be29d646e59c439
Sean Antonio <sean.antonio888@gmail.com>
2026-03-10 15:05:01 +0700

    week06: create Gopay and CreditCard

---
+++ src/oop_113412_SeanAntonio/Week6/PaymentImplementations.kt
@@ -0,0 +1,13 @@
+package oop_113412_SeanAntonio.Week6
+
+class Gopay : PaymentMethod {
+    override fun pay(amount: Double) {
+        println("Processing Rp$amount via Gopay Server")
+    }
+}
+
+class CreditCard : PaymentMethod {
+    override fun pay(amount: Double) {
+        println("Contacting Bank for Rp$amount")
+    }
+}
