aa3a50b21120a5b3ce2d660684f9c992f31e033a
Sean Antonio <sean.antonio888@gmail.com>
2026-03-10 15:31:05 +0700

    week06: (task) instantiate smart devices in main

---
+++ src/oop_113412_SeanAntonio/Week6/Main.kt
@@ -0,0 +1,7 @@
+package oop_113412_SeanAntonio.Week6
+
+fun Main {
+    val lamp = SmartLamp("L1", "Ruang Tamu")
+    val speaker = SmartSpeaker("S1", "Google Nest Dapur")
+    val cctv = SmartCCTV("C1", "Ezviz Garasi")
+}
