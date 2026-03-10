a40163a5dea9b16ebeab5306fa66536f4ed16430
Sean Antonio <sean.antonio888@gmail.com>
2026-03-10 14:57:01 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_113412_SeanAntonio/Week6/GadgetInterface.kt
@@ -0,0 +1,13 @@
+package oop_113412_SeanAntonio.Week6
+
+interface Camera{
+    fun turnOn(){
+        println("Lensa kamera terbuka dan sensor aktif.")
+    }
+}
+
+interface Phone{
+    fun turnOn(){
+        println("Sinyal seluler mencari jaringan.")
+    }
+}
