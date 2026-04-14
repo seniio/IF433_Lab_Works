e307831510a68ba6d06d6c47fefa9cdeec5cc9fa
Sean Antonio <sean.antonio888@gmail.com>
2026-04-14 15:11:32 +0700

    week08: implement safe casting as? to filter collection

--- src/oop_113412_SeanAntonio/Week08/Main.kt
+++ src/oop_113412_SeanAntonio/Week08/Main.kt
@@ -20,4 +20,11 @@ fun main () {
     val mixedData: List<Any> = listOf(
         "Smartphone", 1500000, UserProfile("Andi", null), "Laptop", 4500000.0
     )
+
+    for (item in mixedData) {
+        val text = item as? String // Safe cast as? [cite: 96]
+        text?.let {
+            println("Ditemukan teks: ${it.uppercase()}")
+        }
+    }
 }
