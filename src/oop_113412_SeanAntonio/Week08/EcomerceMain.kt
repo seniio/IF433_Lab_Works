b74d460f477a258f8c1088042d303c6a00412612
Sean Antonio <sean.antonio888@gmail.com>
2026-04-14 15:48:21 +0700

    week08: (task) process mixed raw JSON list using as?

---
+++ src/oop_113412_SeanAntonio/Week08/EcomerceMain.kt
@@ -0,0 +1,12 @@
+package oop_113412_SeanAntonio.Week08
+
+fun main() {
+    val rawApiData: List<Map<String, Any?>> = listOf(
+        mapOf("id" to "E01", "name" to "Laptop", "type" to "ELECTRONIC", "warranty" to 24),
+        mapOf("id" to "C01", "name" to "T-Shirt", "type" to "CLOTHING", "size" to "XL"),
+        mapOf("id" to "E02", "name" to "Mouse", "type" to "ELECTRONIC", "warranty" to "Not An Integer"),
+        mapOf("name" to "Ghost Item", "type" to "CLOTHING"),
+        mapOf("id" to "X01", "name" to "Unknown", "type" to "FOOD")
+    )
+
+}
