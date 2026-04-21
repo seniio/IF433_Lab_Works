4ca1d33578baa2eb5ee379f76ca59f8b791a72fe
Sean Antonio <sean.antonio888@gmail.com>
2026-04-21 15:34:18 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_113412_SeanAntonio/Week09/TradingDashboard.kt
@@ -0,0 +1,12 @@
+package oop_113412_SeanAntonio.Week09
+
+fun main() {
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("BTCUSDT", "SHORT", 50, -10.0, "CLOSED"),
+        TradeLog("SOLUSDT", "LONG", 15, 25.0, "OPEN"),
+        TradeLog("ETHUSDT", "LONG", 20, 40.0, "CLOSED"),
+        TradeLog("BNBUSDT", "SHORT", 10, -2.5, "CLOSED")
+    )
+}
