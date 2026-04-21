068d15c911b5db90804c602bd7d404ed85da5722
Sean Antonio <sean.antonio888@gmail.com>
2026-04-21 15:30:11 +0700

    week09: create Student data class and list

---
+++ src/oop_113412_SeanAntonio/Week09/ObjectChaining.kt
@@ -0,0 +1,15 @@
+package oop_113412_SeanAntonio.Week09
+
+data class Student(
+    val name: String,
+    val gpa: Double
+)
+
+fun main() {
+    val students = listOf(
+        Student("Andi", 3.2),
+        Student("Budi", 3.9),
+        Student("Citra", 2.8),
+        Student("Dewi", 3.7)
+    )
+}
