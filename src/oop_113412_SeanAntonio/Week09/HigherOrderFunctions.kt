6b7b368060c6b8455f1fd2229f34e24355e3a771
Sean Antonio <sean.antonio888@gmail.com>
2026-04-21 15:28:42 +0700

    week09: implement filter HOF to extract data

---
+++ src/oop_113412_SeanAntonio/Week09/HigherOrderFunctions.kt
@@ -0,0 +1,10 @@
+package oop_113412_SeanAntonio.Week09
+
+fun main() {
+    val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8)
+    println("Original Data: $numbers")
+
+    println("\n=== HOF: FILTER ===")
+    val evens = numbers.filter { it % 2 == 0 }
+    println("Evens only: $evens")
+}
