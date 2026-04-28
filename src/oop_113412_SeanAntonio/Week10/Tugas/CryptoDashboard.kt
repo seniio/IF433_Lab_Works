4d92dbaaf6876ec7f63e1db7b44e9509f8214610
Sean Antonio <sean.antonio888@gmail.com>
2026-04-28 08:46:13 +0700

    week10: (task) populate coin repository

--- src/oop_113412_SeanAntonio/Week10/Tugas/CryptoDashboard.kt
+++ src/oop_113412_SeanAntonio/Week10/Tugas/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_113412_SeanAntonio.Week10.Tugas
 
 fun main(){
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 10.0))
+    coinRepo.add(Coin("USDT", 1000.0))
 }
