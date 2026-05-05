361f951d3dc12ec956bb2a5833b9ac7c53fcf688
Sean Antonio <sean.antonio888@gmail.com>
2026-05-05 09:11:58 +0700

    week11: implement run function for object computation

---
+++ src/oop_113412_SeanAntonio/Week11/ScopeConfiguration.kt
@@ -0,0 +1,3 @@
+package oop_113412_SeanAntonio.Week11
+
+data class User(var name: String = "", var age: Int = 0)
--- src/oop_113412_SeanAntonio/Week11/ScopeLetRun.kt
+++ src/oop_113412_SeanAntonio/Week11/ScopeLetRun.kt
@@ -7,6 +7,14 @@ fun main() {
     val length = name?.let {
         println("Nama terdeteksi: $it")
         it.length
-    }
+    } ?:0
+
     println("Panjang nama: $length")
+
+    println("\n=== TEST RUN FUNCTION ===")
+    val result = "Kotlin".run {
+        println("Memproses kata: ${this}")
+        length * 2
+    }
+    println("Hasil kalkulasi run: $result")
 }
