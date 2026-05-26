1db47d83323b454b261b1e4799064c88d5621eda
Sean Antonio <sean.antonio888@gmail.com>
2026-05-26 13:21:44 +0700

    week14: create DiscountStrategy interface and implementations

---
+++ src/oop_113412_SeanAntonio/Week14/LAB/OcpRefactored.kt
@@ -0,0 +1,13 @@
+package oop_113412_SeanAntonio.Week14.LAB
+
+interface DiscountStrategy {
+    fun apply(price: Double): Double
+}
+
+class StudentDiscount : DiscountStrategy {
+    override fun apply(price: Double): Double = price * 0.80
+}
+
+class MemberDiscount : DiscountStrategy {
+    override fun apply(price: Double): Double = price * 0.85
+}
