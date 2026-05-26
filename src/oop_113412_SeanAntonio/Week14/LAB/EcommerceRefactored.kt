6abfd7664b2b2cca90eb6d6b6d93c7ec0bfb316a
Sean Antonio <sean.antonio888@gmail.com>
2026-05-26 14:06:46 +0700

    week14: (task) implement PricingStrategy interface for OCP

--- src/oop_113412_SeanAntonio/Week14/LAB/EcommerceRefactored.kt
+++ src/oop_113412_SeanAntonio/Week14/LAB/EcommerceRefactored.kt
@@ -24,3 +24,31 @@ class EmailNotifier : NotificationService {
         println("Email terkirim: Pesanan $itemName Anda telah dikonfirmasi!")
     }
 }
+
+interface PricingStrategy {
+    val customerType: String
+    fun calculate(price: Double): Double
+}
+
+class RegularPricing : PricingStrategy {
+    override val customerType: String = "REGULAR"
+    override fun calculate(price: Double): Double = price
+}
+
+class VipPricing : PricingStrategy {
+    override val customerType: String = "VIP"
+    override fun calculate(price: Double): Double = price * 0.90
+}
+
+class SafeOrderProcessor(
+    private val repo: OrderRepository,
+    private val notifier: NotificationService
+) {
+    fun processOrder(itemName: String, basePrice: Double, strategy: PricingStrategy) {
+        val finalPrice = strategy.calculate(basePrice)
+        println("Memproses pesanan $itemName seharga $finalPrice")
+
+        repo.saveOrder(itemName, finalPrice, strategy.customerType)
+        notifier.sendNotification(itemName)
+    }
+}
